58881639560f764b248293b2aec883990492b42d
Mikhail Zhelein <mikhail.zhelezin@yandex.ru>
2020-03-28 22:35:10 +0300

    4th task

--- src/com/company/Dictionary.java
+++ src/com/company/Dictionary.java
@@ -1,5 +1,44 @@
 package com.company;
 
-public class Dictionary {
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
+public class Dictionary {
+    private HashMap<Character, Integer> counter;
+    public Dictionary(String inputFile) {
+        try(FileReader reader = new FileReader(inputFile))
+        {
+            counter = new HashMap<>();
+            int c;
+            while((c = reader.read())!=-1){
+                char current = (char)c;
+                if(current <= 'Z' && current >= 'A' || current <= 'z' && current >= 'a') {
+                    if(!counter.containsKey(current)) {
+                        counter.put(current, 1);
+                    }
+                    else{
+                        counter.put(current, counter.get(current) + 1);
+                    }
+                }
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    public void printToFile(String outputFile) {
+        try(FileWriter writer = new FileWriter(outputFile, false))
+        {
+            final String[] output = {""};
+            counter.forEach((k, v) -> output[0] += k + ": " + v + "\n");
+            writer.write(output[0]);
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int[][] firstArr = {{1, 2}, {2, 3}};
-        int[][] secondArr = {{2, 1, 1}, {3, 2, 2}};
-        Matrix m1 = new Matrix(firstArr);
-        Matrix m2 = new Matrix(secondArr);
-        try {
-            m1.add(m2);
-            System.out.print(m1);
-        } catch (Exception e) {
-            System.out.println("Неверная размерность");
-        }
+    private static void workWithDictionary() {
+        Scanner in = new Scanner(System.in);
+        String inputFile = in.next();
+        Dictionary d = new Dictionary(inputFile);
+        String outputFile = in.next();
+        d.printToFile(outputFile);
+    }
 
+    public static void main(String[] args) {
+        workWithDictionary();
     }
 }
